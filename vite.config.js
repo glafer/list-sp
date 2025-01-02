c07cfdd11f6d389212c485f68568adc4645d0bc3
Guillermo Lapuente Fernández <guillermo.lapuente.fernandez@lottoland.com>
2025-01-02 10:49:07 +0100

    added base url to makes gh-pages work propertly

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
 export default defineConfig({
+    base:'/listas-sp/',
     plugins: [preact()],
     build: {
-        outDir: 'dist',
+        outDir: 'public',
         emptyOutDir: true,
     },
 });
